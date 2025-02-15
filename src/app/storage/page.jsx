b2f6cb0c5a3b680e95becfd21831b5f215419ffe
DevExcel Developer <127732089+develeper-devexcelit@users.noreply.github.com>
2025-02-15 16:38:26 +0500

    landing page change

--- src/app/storage/page.jsx
+++ src/app/storage/page.jsx
@@ -2,7 +2,7 @@ import StorageGuide from "@/components/storage/storage";
 
 export default function StoragePage() {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto ">
       <StorageGuide />
     </div>
   );
