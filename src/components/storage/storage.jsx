a8b34c59cd21ad8e35c00d72fd244e25cc0f6c00
faiz toufiq <faiztoufiq123@gmail.com>
2025-02-27 21:49:41 +0500

    changes

--- src/components/storage/storage.jsx
+++ src/components/storage/storage.jsx
@@ -18,7 +18,7 @@ export default function StorageGuide() {
   return (
     <div className="bg-black text-white p-6 max-w-md mx-auto">
       <div className="absolute top-10 cursor-pointer right-10" onClick={handleClose}>
-        <X className="w-6 h-6 text-white" />
+        <X className="w-8 h-8 text-white" />
       </div>
 
       <div className="space-y-8 mt-10">
